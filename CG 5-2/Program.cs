ce88c5a973ffa2c5731035a505915490dedc37f2
bennitalg <bennitalg@outlook.com>
2018-09-05 21:43:11 -0500

    Calculator Week 5

--- CG 5-2/Program.cs
+++ CG 5-2/Program.cs
@@ -6,7 +6,88 @@ namespace CG_5_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calculator");
+            //declare variables 
+
+            double num1;
+            double num2;
+            double total;
+            char oper;
+
+            //ask for user input of the first number
+
+            Console.WriteLine("Enter first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            //ask for user input of the mathmatical operator
+
+            Console.WriteLine("Enter the mathmatical operator: ");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            //ask user input for the second number
+
+            Console.WriteLine("Enter second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            //set an if/else if statement for user to choose to add, subtract, divide or multiply
+
+            if (oper == '+')
+            {
+                total = num1 + num2;
+                Console.WriteLine("Your answer is: " + total);
+            }
+            else if (oper == '-')
+            {
+                total = num1 - num2;
+                Console.WriteLine("Your answer is: " + total);
+            }
+            else if (oper == '*')
+            {
+                total = num1 * num2;
+                Console.WriteLine("Your answer is: " + total);
+            }
+            else if (oper == '/')
+            {
+                total = num1 / num2;
+                Console.WriteLine("Your answer is: " + total);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+
+            }
+            Console.ReadLine();
+        }
+        public class calculator
+        {
+            String name;
+
+            /// <summary>
+            /// text for name 
+            /// </summary> creation of methods for calculator and return a value
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <returns></returns>
+            public int add(int x, int y)
+            {
+                return x + y;
+            }
+            public int subtract (int x, int y)
+            {
+                return x - y;
+            
+            }
+            public int multiply(int x, int y)
+            {
+                return x * y;
+            }
+            public int divide (int x, int y)
+            {
+                return x / y;
+            }
+                      
+                
+            
         }
     }
 }
